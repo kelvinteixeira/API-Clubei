e76f94a1e2580d618590f7f9d15c3b9261af3f14
kelvinbass <kelbass17@gmail.com>
2021-10-01 11:44:39 -0300

    Adicionado Knex ao projeto

---
+++ src/knexfile.ts
@@ -0,0 +1,11 @@
+export default {
+  development: {
+    cliente: 'mysql2',
+    connection: {
+      host: 'localhost',
+      user: 'root',
+      password: 'root',
+      databse: 'ClubeiDB'
+    }
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -4,12 +4,12 @@ import PlanosController from "./controllers/PlanosController"
 
 const routes = Router()
 
-routes.post('/planos', PlanosController.create)
-routes.put('/planos', PlanosController.update)
-routes.get('/planos', PlanosController.list)
+routes.post('/api-clubei/planos', PlanosController.create)
+routes.put('/api-clubei/planos', PlanosController.update)
+routes.get('/api-clubei/planos', PlanosController.list)
 
-routes.post('/assinaturas', AssinaturasController.create)
-routes.get('/assinaturas', AssinaturasController.list)
-routes.put('/assinaturas', AssinaturasController.update)
+routes.post('/api-clubei/assinaturas', AssinaturasController.create)
+routes.get('/api-clubei/assinaturas', AssinaturasController.list)
+routes.put('/api-clubei/assinaturas', AssinaturasController.update)
 
 export default routes
