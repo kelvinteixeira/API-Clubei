23d6aa04ca1ec4999bb1849c6a47ae25094d7b7b
kelvinbass <kelbass17@gmail.com>
2021-10-01 11:05:39 -0300

    Criado metodos das assinaturas

--- src/controllers/AssinaturasController.ts
+++ src/controllers/AssinaturasController.ts
@@ -1,1 +1,44 @@
 import { Request, Response } from "express";
+
+export default {
+  async create(req: Request, res: Response) {
+    const { titulo, descricao, categoria, imagem } = req.body
+    const id = 1
+    const data = { id, titulo, descricao, categoria, imagem }
+    return res.status(201).json({ data: data })
+  },
+
+  async list(req: Request, res: Response) {
+    let assinaturas = [
+      {
+        id: 1,
+        titulo: "Assinatura A",
+        categoria: "Categoria 1",
+        descricao: "Assinatura top...",
+        valor: 49.90
+      },
+      {
+        id: 1,
+        titulo: "Assinatura B",
+        categoria: "Categoria 2",
+        descricao: "Assinatura top...",
+        imagem: "link"
+      },
+      {
+        id: 3,
+        titulo: "Assinatura C",
+        categoria: "Categoria 3",
+        descricao: "Assinatura top...",
+        imagem: "link"
+      },
+    ]
+    return res.status(200).json({data: assinaturas})
+  },
+  async update(req: Request, res: Response) {
+    const { titulo, categoria, descricao, imagem  } = req.body
+    const dataAlteracao = '01/10/2021 16:00'
+    const assinatura = { titulo, descricao, categoria, imagem, dataAlteracao }
+    assinatura.titulo = "Assinatura X"
+    return res.status(200).json({data: assinatura})
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import AssinaturasController from "./controllers/AssinaturasController";
 import PlanosController from "./controllers/PlanosController"
 
 const routes = Router()
@@ -6,5 +7,9 @@ const routes = Router()
 routes.post('/planos', PlanosController.create)
 routes.put('/planos', PlanosController.update)
 routes.get('/planos', PlanosController.list)
+
+routes.post('/assinaturas', AssinaturasController.create)
+routes.get('/assinaturas', AssinaturasController.list)
+routes.put('/assinaturas', AssinaturasController.update)
 
 export default routes
