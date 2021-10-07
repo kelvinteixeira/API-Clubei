455018a13a4bf7ffee61333a0fca317ec4ab8544
kelvinbass <kelbass17@gmail.com>
2021-10-07 12:54:33 -0300

    Vinculado ao SQLtools

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -4,17 +4,11 @@ export default {
   development: {
     client: 'mysql2',
     connection: {
-      host: 'localhost',
-      user: 'root',
-      password: 'root',
-      database: 'clubei_db'
-    },
-    migrations: {
-      tableName: 'knex-migrations',
-      directory: path.resolve(__dirname, 'src', 'database', 'migrations')
-    },
-    seeds: {
-      directory: path.resolve(__dirname, 'src', 'database', 'seeds')
+      host: 'us-cdbr-east-04.cleardb.com',
+      user: 'ba74bf79fcfadb',
+      password: 'aa3810d4',
+      database: 'heroku_2a2856ff6780273',
+      port: '3306'
     }
   }
 }
