0f6e2f34e4d0be54056f8213dbfa7185df86cdb1
kelvinbass <kelbass17@gmail.com>
2021-10-07 20:30:24 -0300

    corrigido erro no knexfile

--- dist/knexfile.js
+++ dist/knexfile.js
@@ -5,9 +5,10 @@ exports.default = {
         client: 'mysql2',
         connection: {
             host: 'us-cdbr-east-04.cleardb.com',
-            user: '"ba74bf79fcfadb',
+            user: 'ba74bf79fcfadb',
             password: 'aa3810d4',
             database: 'heroku_2a2856ff6780273',
+            port: 3306
         }
     }
 };
--- src/knexfile.ts
+++ src/knexfile.ts
@@ -5,7 +5,7 @@ export default {
     client: 'mysql2',
     connection: {
       host: 'us-cdbr-east-04.cleardb.com',
-      user: '"ba74bf79fcfadb',
+      user: 'ba74bf79fcfadb',
       password: 'aa3810d4',
       database: 'heroku_2a2856ff6780273',
       port: 3306
