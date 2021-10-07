4ef84eca3e7b60c65329c2155857fbad432719f4
kelvinbass <kelbass17@gmail.com>
2021-10-07 20:45:18 -0300

    alterado path das rotas

--- src/routes.ts
+++ src/routes.ts
@@ -17,16 +17,16 @@ const upload = multer({ storage: storage })
 
 const routes = Router()
 
-routes.post('/api-clubei/assinaturas', upload.single('assinatura_img'), AssinaturasController.create)
-routes.get('/api-clubei/assinaturas', AssinaturasController.list)
-routes.get('/api-clubei/assinaturas/:id', AssinaturasController.find)
-routes.put('/api-clubei/assinaturas/:id', AssinaturasController.update)
-routes.delete('/api-clubei/assinaturas/:id', AssinaturasController.delete)
+routes.post('/assinaturas', upload.single('assinatura_img'), AssinaturasController.create)
+routes.get('/assinaturas', AssinaturasController.list)
+routes.get('/assinaturas/:id', AssinaturasController.find)
+routes.put('/assinaturas/:id', AssinaturasController.update)
+routes.delete('/assinaturas/:id', AssinaturasController.delete)
 
-routes.post('/api-clubei/categorias', CategoriasController.create)
-routes.get('/api-clubei/categorias', CategoriasController.list)
-routes.get('/api-clubei/categorias/:id_categoria/assinaturas', CategoriasController.find)
-routes.put('/api-clubei/categorias/:id_categoria/', CategoriasController.update)
-routes.delete('/api-clubei/categorias/:id_categoria', CategoriasController.delete)
+routes.post('/categorias', CategoriasController.create)
+routes.get('/categorias', CategoriasController.list)
+routes.get('/categorias/:id_categoria/assinaturas', CategoriasController.find)
+routes.put('/categorias/:id_categoria/', CategoriasController.update)
+routes.delete('/categorias/:id_categoria', CategoriasController.delete)
 
 export default routes
