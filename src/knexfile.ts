79480a7feaad2a79192e5b7c32fe761dd0166a9c
kelvinbass <kelbass17@gmail.com>
2021-10-07 13:17:01 -0300

    alterado credenciais do db

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -4,11 +4,11 @@ export default {
   development: {
     client: 'mysql2',
     connection: {
-      host: 'us-cdbr-east-04.cleardb.com',
-      user: 'ba74bf79fcfadb',
-      password: 'aa3810d4',
-      database: 'heroku_2a2856ff6780273',
-      port: '3306'
+      host: 'localhost',
+      user: 'root',
+      password: 'root',
+      database: 'cluei_db',
+      
     }
   }
 }
