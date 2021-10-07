261ba16efc7f4eb100485134376b4b73e20af04f
kelvinbass <kelbass17@gmail.com>
2021-10-06 21:23:10 -0300

    Removido o procfile e adicionado o script start

--- dist/database/connection.js
+++ dist/database/connection.js
@@ -4,6 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var knex_1 = __importDefault(require("knex"));
-var knexfile_1 = __importDefault(require("../../knexfile"));
+var knexfile_1 = __importDefault(require("../knexfile"));
 exports.default = (0, knex_1.default)(knexfile_1.default['development']);
 console.log('Conexão realizada com sucesso');
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,5 +1,5 @@
 import knex from 'knex'
-import knexfile from '../../knexfile'
+import knexfile from '../knexfile'
 
 export default knex(knexfile['development'])
 console.log('Conexão realizada com sucesso')
