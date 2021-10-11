b1bbcfb040c8f146dae47fe9ff7643aff7a402fe
kelvinbass <kelbass17@gmail.com>
2021-10-11 13:45:36 -0300

    switch case finalizado

--- dist/controllers/AssinaturasController.js
+++ dist/controllers/AssinaturasController.js
@@ -103,16 +103,36 @@ exports.default = {
                         switch (id_categoria) {
                             case "1":
                                 return [2 /*return*/, res.status(200).json({
-                                        message: "Categoria de Alimentos",
-                                        assinaturaCategoria: assinaturaCategoria
+                                        Categoria: "Alimentos",
+                                        data: assinaturaCategoria
                                     })];
                                 break;
                             case "2":
                                 return [2 /*return*/, res.status(200).json({
-                                        message: "Categoria de Arte e Culturas",
-                                        assinaturaCategoria: assinaturaCategoria
+                                        Categoria: "Arte e Culturas",
+                                        data: assinaturaCategoria
                                     })];
                                 break;
+                            case "3":
+                                return [2 /*return*/, res.status(200).json({
+                                        Categoria: "Bebidas",
+                                        data: assinaturaCategoria
+                                    })];
+                                break;
+                            case "4":
+                                return [2 /*return*/, res.status(200).json({
+                                        Categoria: "Fitness",
+                                        data: assinaturaCategoria
+                                    })];
+                                break;
+                            case "5":
+                                return [2 /*return*/, res.status(200).json({
+                                        Categoria: "LIvros",
+                                        data: assinaturaCategoria
+                                    })];
+                                break;
+                            default:
+                                return [2 /*return*/, res.status(404).json({ message: "Categoria inexistente ou não encontrada" })];
                         }
                         return [2 /*return*/];
                 }
