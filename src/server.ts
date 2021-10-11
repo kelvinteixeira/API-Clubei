f4d2c3c4061f29d739c86d81b11d9643fe04a917
kelvinbass <kelbass17@gmail.com>
2021-10-11 15:47:53 -0300

    corrigido erro de rota

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app = express()
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000
 
 app.use(express.json())
-app.use(routes)
+app.use('/v1',routes)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use('/uploads/', express.static('uploads'))
 
