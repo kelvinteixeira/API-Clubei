29447a091accb312584036f945bf02f9cb9dd97d
kelvinbass <kelbass17@gmail.com>
2021-10-11 15:54:06 -0300

    corrigido erro de rota

--- dist/server.js
+++ dist/server.js
@@ -10,7 +10,7 @@ var swagger_json_1 = __importDefault(require("./swagger.json"));
 var app = (0, express_1.default)();
 var PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 app.use(express_1.default.json());
-app.use(routes_1.default);
+app.use('/v1', routes_1.default);
 app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default));
 app.use('/uploads/', express_1.default.static('uploads'));
 app.listen(PORT, function () {
@@ -18,6 +18,6 @@ app.listen(PORT, function () {
 });
 app.get('/', function (req, res) {
     return res.json({
-        message: 'Bem vindo a REST-API de consulta do Clubei, está API foi desenvolvida para o site Clubei, projeto final do Gama Xp 38ª edição. Para consultar mais informações e os endpoints, acesse nossa documentação digitando ao fim da barra de pesquisa "/api-docs"'
+        message: 'Bem vindo a REST-API de consulta do Clubei, está API foi desenvolvida para o site Clubei, projeto final do Gama Xp 38ª edição. Para consultar mais informações e os endpoints, acesse nossa documentação digitando ao fim da barra de pesquisa api-docs'
     });
 });
